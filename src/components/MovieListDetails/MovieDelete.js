641e8ab3da56b57172d249fe340ba6ff6e6cac61
Andrii Hrytsai <andriyhrytsay@gmail.com>
2021-05-18 15:43:56 +0300

    create movie project

--- src/components/MovieListDetails/MovieDelete.js
+++ src/components/MovieListDetails/MovieDelete.js
@@ -1,13 +1,14 @@
-export default function MovieDelete({value}) {
-    return (
-        <div>
-
-            <div className={"likeContainer"} key={value.id}>
-                <img className={"likePhoto"} src={"https://image.tmdb.org/t/p/w200" + value.photo} alt="likePhoto"/>
-                <p className={"likeTitle"}>{value.name}</p>
-                <button className={"likeButton"} style={{background: "red"}}>Delete</button>
-            </div>
-
-        </div>
-    )
-}
+// export default function MovieDelete({value}) {
+//     console.log(value)
+//     return (
+//         <div>
+//
+//             <div className={"likeContainer"} key={value.id}>
+//                 <img className={"likePhoto"} src={"https://image.tmdb.org/t/p/w200" + value.photo} alt="likePhoto"/>
+//                 <p className={"likeTitle"}>{value.name}</p>
+//                 <button className={"likeButton"} style={{background: "red"}}>Delete</button>
+//             </div>
+//
+//         </div>
+//     )
+// }
--- src/components/MovieListDetails/MovieLike.js
+++ src/components/MovieListDetails/MovieLike.js
@@ -1,16 +1,27 @@
 import "./MovieListDetails.css"
+import {useState} from "react";
 
 export default function MovieLike() {
     let likesMovie = JSON.parse(localStorage.getItem("like"))
-    console.log(likesMovie)
+    let [newLike, setNewLike] = useState(likesMovie)
+    const delLike = (id) => {
+        setNewLike(likesMovie.filter(value => value.id !== id))
+    }
+    localStorage.setItem("like", JSON.stringify(newLike))
+    // console.log(likesMovie)
     return (
         <div className={"likeWrapper"}>
             <div className={"likeWrapperContainer"}>
                 {
-                    likesMovie && likesMovie.map(value => <div className={"likeContainer"} key={value.id}>
+                    newLike && newLike.map(value => <div className={"likeContainer"} key={value.id}>
                         <img className={"likePhoto"} src={"https://image.tmdb.org/t/p/w200" + value.photo}
                              alt="posterPath"/>
                         <p className={"likeTitle"}> {value.title}</p>
+                        <button
+                            className={"delLikeMovie"} onClick={() => {
+                            delLike(value.id)
+                        }}>X
+                        </button>
                     </div>)
                 }
             </div>
